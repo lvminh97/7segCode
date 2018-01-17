20dc5d5d692dea48ef9d0838067d6706ee4b3422
Minh Luong Van <32778047+lvminh97@users.noreply.github.com>
2018-01-18 00:57:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main() {
 	_7seg obj(350, 450, "Code7Seg - John MacTavish");
 	while(true) {
-        obj.typeDisp();
-        obj.ledDisp();
-        obj.codeDisp();
-        obj.mouseEvent();
+        	obj.typeDisp();
+        	obj.ledDisp();
+        	obj.codeDisp();
+        	obj.mouseEvent();
 		delay(10);
 	}
 }
